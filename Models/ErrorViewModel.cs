df50c3fabf7ba316463b088ad78af4c9d6fbf992
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2023-04-03 10:56:15 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Lab_evaluation_Capstone.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
