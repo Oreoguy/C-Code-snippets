b630fed4930ea386978f542728520593e44c3a90
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2023-07-04 10:39:38 +0530

    Calculator

---
+++ C++/countrycode.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int code;
+cout<<"Enter the country code.";
+cin>>code;
+if(code==93)
+{
+    cout<<"Afghanistan";
+}
+else if (code==44)
+{
+    cout<<"UK";
+}
+else if (code==91)
+{
+    cout<<"India";
+}
+else
+{
+    cout<<"Any country";
+}
+}
