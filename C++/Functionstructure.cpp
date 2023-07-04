b6832c3c788016db7494e3de8fad9b7764088535
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2023-07-04 11:34:45 +0530

    Function and matrix structure

---
+++ C++/Functionstructure.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int age, salary;
+    string name;
+void accept();
+void display();
+int main()
+{
+    accept();
+    display();
+    return 0;
+
+}
+
+void accept(){
+
+    cout<<"Enter name ,age and salary:";
+    
+}
