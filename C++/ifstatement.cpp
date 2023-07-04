1940855603d7d89a4de5c0961cb3455daf46e5a8
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2023-07-04 10:16:38 +0530

    C++

---
+++ C++/ifstatement.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter age:";
+    cin>>age;
+    if(age>18)
+    {
+
+        cout<<"You are eligible to vote \n";
+    }
+    else 
+    {
+        cout<<"You are not eligible to vote";
+    }
+    cout<<"\n Out of Blocks.";
+}
+
