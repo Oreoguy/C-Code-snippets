45b1acec3cbae3a0ad7527e3902f56b311bd3ee5
Vansh Bhatnagar <99128036+Oreoguy@users.noreply.github.com>
2024-02-21 15:03:14 +0530

    Encryption1

---
+++ C++/Encryptionandsecurityconcepts/encrypt.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+using namespace std;
+struct encryption
+{
+    char enc[200];
+    char dec[200];
+    int key;
+    char text[200];
+};
+
+int main()
+{
+    struct encryption e;
+    string uname="Vansh",pass="v1234h";
+    string username, password;
+    char ch;
+    int unamecheck, passwordcheck;
+    int i,op;
+    cout <<"\n\t\t\t\t------+SECURITY OF YOUR DATA MATTERS+-------\n";
+    cout <<"\n\t\t\t\t-------PLEASE LOG IN TO USE FEATURES--------\n";
+    cout <<"\n--------------------\n";
+    cout <<"\t ENTER USERNAME";
+    cin>> username;
+    cout <<"\t ENTER PASSWORD";
+    cin>> password;
+    
+}
